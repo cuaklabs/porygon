a67812efdff554b225df1b1d2aacc0e0819ed210
notaphplover <roberto.pintos.lopez@gmail.com>
2022-12-31 18:17:59 +0100

    feat(porygon-common): add Converter types

---
+++ packages/porygon-common/src/index.ts
@@ -0,0 +1,4 @@
+import { Converter } from './foundation/modules/domain/Converter';
+import { ConverterAsync } from './foundation/modules/domain/ConverterAsync';
+
+export type { Converter, ConverterAsync };
