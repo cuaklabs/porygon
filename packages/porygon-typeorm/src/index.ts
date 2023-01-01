876f444007724f036523a61a66bd16b6603af996
notaphplover <roberto.pintos.lopez@gmail.com>
2023-01-01 12:13:08 +0100

    feat(porygon-typeorm): expose UpdateTypeOrmService

--- packages/porygon-typeorm/src/index.ts
+++ packages/porygon-typeorm/src/index.ts
@@ -1,5 +1,11 @@
 import { DeleteTypeOrmService } from './persistence/services/typeorm/DeleteTypeOrmService';
 import { FindTypeOrmService } from './persistence/services/typeorm/FindTypeOrmService';
 import { InsertTypeOrmService } from './persistence/services/typeorm/InsertTypeOrmService';
+import { UpdateTypeOrmService } from './persistence/services/typeorm/UpdateTypeOrmService';
 
-export { DeleteTypeOrmService, FindTypeOrmService, InsertTypeOrmService };
+export {
+  DeleteTypeOrmService,
+  FindTypeOrmService,
+  InsertTypeOrmService,
+  UpdateTypeOrmService,
+};
