3dcdb38d8e97bb817471ef83823274bed549acae
notaphplover <roberto.pintos.lopez@gmail.com>
2023-02-27 02:21:04 +0100

    refactor(porygon-typeorm): add buildPorygonTypeOrmSourceModule

---
+++ packages/porygon-typeorm/src/persistence/builders/buildPorygonTypeOrmSourceModule.spec.ts
@@ -0,0 +1,170 @@
+import { afterAll, beforeAll, describe, expect, it, jest } from '@jest/globals';
+
+jest.mock('typeorm');
+
+import {
+  BindValueOptions,
+  ContainerModule,
+  ContainerModuleBindingService,
+} from '@cuaklabs/iocuak';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+import { PorygonTypeOrmSourceModuleSymbolsMap } from '../models/typeorm/PorygonTypeOrmSourceModuleSymbolsMap';
+import { buildPorygonTypeOrmSourceModule } from './buildPorygonTypeOrmSourceModule';
+
+describe(buildPorygonTypeOrmSourceModule.name, () => {
+  let dataSourceOptionsFixture: DataSourceOptions;
+
+  beforeAll(() => {
+    dataSourceOptionsFixture = Symbol() as unknown as DataSourceOptions;
+  });
+
+  describe('having a PorygonTypeOrmSourceModuleSymbolsMap', () => {
+    let porygonTypeOrmSourceModuleSymbolsMapFixture: PorygonTypeOrmSourceModuleSymbolsMap;
+
+    beforeAll(() => {
+      porygonTypeOrmSourceModuleSymbolsMapFixture = {
+        alias: 'alias',
+        dataSource: Symbol(),
+      };
+    });
+
+    describe('when called', () => {
+      let dataSourceFixture: DataSource;
+      let containerModule: ContainerModule;
+      let result: unknown;
+
+      beforeAll(async () => {
+        dataSourceFixture = {
+          initialize: jest.fn().mockReturnThis(),
+        } as Partial<DataSource> as DataSource;
+
+        (DataSource as jest.Mock).mockReturnValueOnce(dataSourceFixture);
+
+        containerModule = await buildPorygonTypeOrmSourceModule(
+          dataSourceOptionsFixture,
+          porygonTypeOrmSourceModuleSymbolsMapFixture,
+        );
+
+        result = containerModule;
+      });
+
+      afterAll(() => {
+        jest.clearAllMocks();
+      });
+
+      it('should call new DataSource', () => {
+        expect(DataSource).toHaveBeenCalledTimes(1);
+        expect(DataSource).toHaveBeenCalledWith(dataSourceOptionsFixture);
+      });
+
+      it('should call dataSource.initialize()', () => {
+        expect(dataSourceFixture.initialize).toHaveBeenCalledTimes(1);
+        expect(dataSourceFixture.initialize).toHaveBeenCalledWith();
+      });
+
+      it('should return a ContainerModule', () => {
+        const expected: ContainerModule = {
+          load: expect.any(Function) as unknown as (
+            containerModuleBindingService: ContainerModuleBindingService,
+          ) => void,
+        };
+
+        expect(result).toStrictEqual(expected);
+      });
+
+      describe('when called containerModule.load()', () => {
+        let containerModuleBindingServiceMock: jest.Mocked<ContainerModuleBindingService>;
+
+        beforeAll(() => {
+          containerModuleBindingServiceMock = {
+            bindToValue: jest.fn(),
+          } as Partial<
+            jest.Mocked<ContainerModuleBindingService>
+          > as jest.Mocked<ContainerModuleBindingService>;
+
+          containerModule.load(containerModuleBindingServiceMock);
+        });
+
+        afterAll(() => {
+          jest.clearAllMocks();
+        });
+
+        it('should call containerModuleBindingService.bindToValue()', () => {
+          const bindValueOptions: BindValueOptions = {
+            serviceId: porygonTypeOrmSourceModuleSymbolsMapFixture.dataSource,
+            value: dataSourceFixture,
+          };
+
+          expect(
+            containerModuleBindingServiceMock.bindToValue,
+          ).toHaveBeenCalledTimes(1);
+          expect(
+            containerModuleBindingServiceMock.bindToValue,
+          ).toHaveBeenCalledWith(bindValueOptions);
+        });
+      });
+    });
+  });
+
+  describe('having another PorygonTypeOrmSourceModuleSymbolsMap', () => {
+    let porygonTypeOrmSourceModuleSymbolsMapFixture: PorygonTypeOrmSourceModuleSymbolsMap;
+
+    beforeAll(() => {
+      porygonTypeOrmSourceModuleSymbolsMapFixture = {
+        alias: 'another-alias',
+        dataSource: Symbol(),
+      };
+    });
+
+    describe('when called twice', () => {
+      let dataSourceFixture: DataSource;
+      let containerModule: ContainerModule;
+      let result: unknown;
+
+      beforeAll(async () => {
+        dataSourceFixture = {
+          initialize: jest.fn().mockReturnThis(),
+        } as Partial<DataSource> as DataSource;
+
+        (DataSource as jest.Mock).mockReturnValueOnce(dataSourceFixture);
+
+        containerModule = await buildPorygonTypeOrmSourceModule(
+          dataSourceOptionsFixture,
+          porygonTypeOrmSourceModuleSymbolsMapFixture,
+        );
+
+        await buildPorygonTypeOrmSourceModule(
+          dataSourceOptionsFixture,
+          porygonTypeOrmSourceModuleSymbolsMapFixture,
+        );
+
+        result = containerModule;
+      });
+
+      afterAll(() => {
+        jest.clearAllMocks();
+      });
+
+      it('should call new DataSource', () => {
+        expect(DataSource).toHaveBeenCalledTimes(1);
+        expect(DataSource).toHaveBeenCalledWith(dataSourceOptionsFixture);
+      });
+
+      it('should call dataSource.initialize()', () => {
+        expect(dataSourceFixture.initialize).toHaveBeenCalledTimes(1);
+        expect(dataSourceFixture.initialize).toHaveBeenCalledWith();
+      });
+
+      it('should return a ContainerModule', () => {
+        const expected: ContainerModule = {
+          load: expect.any(Function) as unknown as (
+            containerModuleBindingService: ContainerModuleBindingService,
+          ) => void,
+        };
+
+        expect(result).toStrictEqual(expected);
+      });
+    });
+  });
+});
---
+++ packages/porygon-typeorm/src/persistence/builders/buildPorygonTypeOrmSourceModule.ts
@@ -0,0 +1,46 @@
+import {
+  ContainerModule,
+  ContainerModuleBindingService,
+} from '@cuaklabs/iocuak';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+import { PorygonTypeOrmSourceModuleSymbolsMap } from '../models/typeorm/PorygonTypeOrmSourceModuleSymbolsMap';
+
+export async function buildPorygonTypeOrmSourceModule(
+  dataSourceOptions: DataSourceOptions,
+  porygonTypeOrmSourceModuleSymbolsMap: PorygonTypeOrmSourceModuleSymbolsMap,
+): Promise<ContainerModule> {
+  const dataSource: DataSource = await buildDatasource(
+    porygonTypeOrmSourceModuleSymbolsMap.alias,
+    dataSourceOptions,
+  );
+
+  const containerModule: ContainerModule = {
+    load: (containerModuleBindingService: ContainerModuleBindingService) => {
+      containerModuleBindingService.bindToValue({
+        serviceId: porygonTypeOrmSourceModuleSymbolsMap.dataSource,
+        value: dataSource,
+      });
+    },
+  };
+
+  return containerModule;
+}
+
+const ALIAS_TO_DATASOURCE_MAP: Map<string, Promise<DataSource>> = new Map();
+
+async function buildDatasource(
+  alias: string,
+  dataSourceOptions: DataSourceOptions,
+): Promise<DataSource> {
+  let dataSourcePromise: Promise<DataSource> | undefined =
+    ALIAS_TO_DATASOURCE_MAP.get(alias);
+
+  if (dataSourcePromise === undefined) {
+    dataSourcePromise = new DataSource(dataSourceOptions).initialize();
+
+    ALIAS_TO_DATASOURCE_MAP.set(alias, dataSourcePromise);
+  }
+
+  return dataSourcePromise;
+}
